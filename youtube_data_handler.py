71e3ae958e93fe344c91ef1aa9850cc492be8753
Vaishnav Bharat Bhor <129935904+lomduhaitu@users.noreply.github.com>
2025-05-03 01:14:48 +0530

    Update youtube_data_handler.py

--- youtube_data_handler.py
+++ youtube_data_handler.py
@@ -5,4 +5,6 @@ def fetch_all_data(topic):
     video_df = pd.read_csv("processed_metadata.csv")
     df = pd.read_csv("comments_flat.csv")
     comments = df["comment_text"].fillna("").astype(str).tolist()
+    comments = comments[:200]  # Limit for speed
+
     return video_df, comments
