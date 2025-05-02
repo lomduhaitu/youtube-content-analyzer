ad4b850ce1585090d49e07903f4255b9bdda720b
Vaishnav Bharat Bhor <129935904+lomduhaitu@users.noreply.github.com>
2025-05-02 23:29:41 +0530

    Update youtube_data_handler.py

--- youtube_data_handler.py
+++ youtube_data_handler.py
@@ -1,8 +1,6 @@
 # youtube_data_handler.py
 import pandas as pd
 
-API_KEY = st.secrets["youtube_api_key"]
-
 def fetch_all_data(topic):
     video_df = pd.read_csv("processed_metadata.csv")
     df = pd.read_csv("comments_flat.csv")
